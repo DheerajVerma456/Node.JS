905a44f9db9c07e9796f7301c64512ade3e3c3fe
Dheeraj Verma <dheerajverma55569@gmail.com>
2023-05-15 17:40:18 +0530

    initial

---
+++ controllers/user.js
@@ -0,0 +1,9 @@
+import {User} from "../modals/user.js"
+
+export const getAllUsers = async (req,res) => {};
+
+export const login = async(req, res, next) => {};
+
+export const register = async(req, res) => {};
+
+export const getUserDetails = async (req, res) =>{};
---
+++ routes/user.js
@@ -0,0 +1,14 @@
+import express, { Router } from "express";
+import {getAllUsers, getUserDetails, login, register} from "../controllers/user.js";
+
+const router = express.Router();
+
+router.get("/all",getAllUsers);
+
+router.post("/new", register);
+router.post("/login", login);
+
+router.route("./userid/:id").get(getUserDetails);
+
+export default router;
+
